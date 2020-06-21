498bbc494d57b260162797c3ab1db4aa7f674f52
Tuukka <tuukka@pop-os.localdomain>
2020-06-21 22:43:49 +0300

    deleted some stuff

---
+++ build/precache-manifest.35686c39cbcedf73b3fe84740479bce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5f05fb104c85da7dcc3b6db4613826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b9b3b176ce30ec21184",
+    "url": "/static/css/main.6e5520d0.chunk.css"
+  },
+  {
+    "revision": "0d0e4a7843afe9a3c41d",
+    "url": "/static/js/2.118405a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.118405a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9b3b176ce30ec21184",
+    "url": "/static/js/main.a3e6c1bc.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
